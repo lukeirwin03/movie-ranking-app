67bc06169a17ac8b3a4354e8bb51efb407938b33
Tom Nguyen <69921672+Suka7u@users.noreply.github.com>
2024-12-03 16:19:01 -0600

    updated css

--- movie-ranker/src/App.js
+++ movie-ranker/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ReactNotifications, Store } from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css'
 import axios from 'axios';
 import './App.css';
 
